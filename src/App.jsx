3602dc71d7ba7c65737111dd246eea05c9d0ef6d
Anoopka19 <142653103+Anoopka19@users.noreply.github.com>
2025-06-03 18:54:50 +0530

    React Movie App (built by following YouTube tutorial), with search, API integration, and favorites

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./css/App.css";
+import NavBar from "./components/NavBar";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./components/contexts/MovieContext";
+function App() {
+  return (
+    <>
+      <div>
+        <MovieProvider>
+          <NavBar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+          </main>
+        </MovieProvider>
+      </div>
+    </>
+  );
+}
+
+export default App;
